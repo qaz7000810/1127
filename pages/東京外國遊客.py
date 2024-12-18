f15da87af21f088845bfcbb3569b3be7436f5034
qaz7000810 <115362593+qaz7000810@users.noreply.github.com>
2024-12-18 14:44:24 +0800

    Update 東京外國遊客.py

--- pages/東京外國遊客.py
+++ pages/東京外國遊客.py
@@ -5,9 +5,9 @@ import pandas as pd
 csv_url = 'https://www.opendata.metro.tokyo.lg.jp/sangyouroudou/behavioral_characteristics_survey/H31R1behavioral_characteristics_survey1.csv'
 
 # 讀取 CSV 文件
-@st.cache
+@st.cache_data
 def load_data(url):
-    data = pd.read_csv(url)
+    data = pd.read_csv(url, encoding='shift_jis')  # 使用正確的編碼來讀取 CSV 文件
     return data
 
 data = load_data(csv_url)
@@ -17,4 +17,3 @@ st.title('行為特徵調查數據')
 
 # 顯示數據表格
 st.dataframe(data)
-
