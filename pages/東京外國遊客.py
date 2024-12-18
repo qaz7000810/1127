cb6398f2d22ba0b006bb05c652ea5c566539db5b
qaz7000810 <115362593+qaz7000810@users.noreply.github.com>
2024-12-18 16:47:24 +0800

    Update 東京外國遊客.py

--- pages/東京外國遊客.py
+++ pages/東京外國遊客.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import chardet
 
 # 定義 CSV 文件的 URL
 csv_url = 'https://github.com/qaz7000810/tower/raw/refs/heads/main/%E8%A7%80%E5%85%89%E5%AE%A2%E6%80%A7%E5%88%A5.csv'
@@ -8,13 +7,8 @@ csv_url = 'https://github.com/qaz7000810/tower/raw/refs/heads/main/%E8%A7%80%E5%
 # 讀取 CSV 文件
 @st.cache_data
 def load_data(url):
-    # 自動檢測編碼
-    with open(url, 'rb') as file:
-        result = chardet.detect(file.read())
-    encoding = result['encoding']
-    
-    # 使用檢測到的編碼來讀取 CSV 文件
-    data = pd.read_csv(url, encoding=encoding)
+    # 讀取並解碼 CSV 文件
+    data = pd.read_csv(url, encoding='shift_jis')
     return data
 
 data = load_data(csv_url)
